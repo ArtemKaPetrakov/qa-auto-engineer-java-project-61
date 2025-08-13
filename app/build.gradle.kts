84de90ad27fa3b24ec60aebdceacfe7dbdd73126
Artem Petrakov <artem.petrakov@climb-lab.ru>
2025-08-13 20:58:58 +0300

    step 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,10 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
 tasks.test {
     useJUnitPlatform()
 }
