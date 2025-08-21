0c46e10958aeeb07d0a21bc57c59caa560b6eb5e
Artem Petrakov <artem.petrakov@climb-lab.ru>
2025-08-22 01:07:02 +0300

    fix7 EVEN

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -8,12 +8,12 @@ public class Even {
 
     public static void evenGame() {
 
-        int randomMax = 10;
+        private static final int MAX_RANDOM_NUMBER = 10;
 
         String[][] questionsAndAnswers = new String[Engine.GAME_ROUNDS][2];
 
         for (int i = 0; i < Engine.GAME_ROUNDS; i++) {
-            int question = GetRandom.generateRandomNumber(randomMax);
+            int question = GetRandom.generateRandomNumber(MAX_RANDOM_NUMBER);
             String correctAnswer = question % 2 == 0 ? "yes" : "no";
             questionsAndAnswers[i][0] = Integer.toString(question);
             questionsAndAnswers[i][1] = correctAnswer;
